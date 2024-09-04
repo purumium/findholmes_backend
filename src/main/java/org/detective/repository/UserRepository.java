df2ac8781cc8b7bb133d2f3cf610f885fd8f83fc
purumium <129027316+purumium@users.noreply.github.com>
2024-09-04 10:37:28 +0900

    Delete src directory

--- src/main/java/org/detective/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.detective.repository;
-
-import org.detective.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}

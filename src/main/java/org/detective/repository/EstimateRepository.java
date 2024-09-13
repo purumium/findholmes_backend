69dd9dda030bf9cce37c7ebcc27721c30cc73e83
hyl27 <100427hyl@gmail.com>
2024-09-13 09:57:09 +0900

    repository

--- src/main/java/org/detective/repository/EstimateRepository.java
+++ src/main/java/org/detective/repository/EstimateRepository.java
@@ -5,6 +5,7 @@ import org.detective.entity.Detective;
 import org.detective.entity.Estimate;
 import org.detective.entity.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 

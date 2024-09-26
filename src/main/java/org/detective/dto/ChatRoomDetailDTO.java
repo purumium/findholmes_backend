67f1b78eface7c47f8ce50208e18bc88eff81aa3
hyl27 <100427hyl@gmail.com>
2024-09-26 09:53:37 +0900

    chatroom username

--- src/main/java/org/detective/dto/ChatRoomDetailDTO.java
+++ src/main/java/org/detective/dto/ChatRoomDetailDTO.java
@@ -4,12 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ParticipantDTO {
-
-    private Long userId;
-    private String userName;
-    private String role;
+public class ChatRoomDetailDTO {
+    private String chatRoomId;
+    private List<ParticipantDTO> participants;
 }

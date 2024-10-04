c58a11c0544bf5abeb10940e142ffe4b45efa482
ChoiYH96 <169019916+ChoiYH96@users.noreply.github.com>
2024-10-04 17:56:13 +0900

    찐찐막
    feat: 알림 수정, 채팅 알림 수정

--- src/main/java/org/detective/controller/DeletePlz/DeletePlzController.java
+++ src/main/java/org/detective/controller/DeletePlz/DeletePlzController.java
@@ -5,6 +5,9 @@ import org.detective.entity.Detective;
 import org.detective.entity.User;
 import org.detective.repository.DetectiveRepository;
 import org.detective.repository.UserRepository;
+import org.detective.services.member.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +16,35 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+// 유저 50명 생성, 생성한 유저 탐정데이터 넣는 메서드
+// 삭제해도 됨
 @RestController
 @RequestMapping("/plz")
 public class DeletePlzController {
     private final UserRepository userRepository;
     private final DetectiveRepository detectiveRepository;
+    private final UserService userService;
+    private PasswordEncoder passwordEncoder;
 
-    public DeletePlzController(UserRepository userRepository, DetectiveRepository detectiveRepository) {
+    public DeletePlzController(UserRepository userRepository, DetectiveRepository detectiveRepository, UserService userService) {
         this.userRepository = userRepository;
         this.detectiveRepository = detectiveRepository;
+        this.userService = userService;
+    }
+
+    @PostMapping("/random")
+    public void registerUserRandom() {
+        String password = passwordEncoder.encode("1");
+        for (int i = 1; i<51 ; i++) {
+            String userName="탐정"+i;
+            String email = "detective"+i+"@test.com";
+            String phoneNumber = i>9?"010-0000-00"+i:"010-0000-000"+i;
+            String role = "ROLE_DETECTIVE";
+            User user = new User(userName, email, phoneNumber,password,role);
+
+            userRepository.save(user);
+
+        }
     }
 
     @PostMapping("detective")
